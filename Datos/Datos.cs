4f444e86d773b68ef0535add4317c07458277404
Emiliano Ruiz Orantes <emilianoro20@outlook.com>
2025-09-09 21:19:28 -0600

    Agregar archivos de proyecto.

---
+++ Datos/Datos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class Datos
+    {
+        string nombre;
+        int edad;
+        float peso = 0f;
+        public void pedir()
+        {
+            Console.WriteLine("Ingresa tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingresa tu edad");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa tu peso");
+            peso = float.Parse(Console.ReadLine());
+
+        }
+        public void devolver()
+        {
+            Console.WriteLine("Tu nombre es: " + nombre);
+            Console.WriteLine("Tu edad es: " + edad);
+            Console.WriteLine("Tu peso es: " + peso);
+        }
+    }
+}
---
+++ Datos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos objdatos = new Datos();
+            string universidad;
+            Console.WriteLine("Ingresa tu universidad: ");
+            universidad = Console.ReadLine();
+            objdatos.pedir();
+            objdatos.devolver();
+            Console.WriteLine("Tu universidad es: " + universidad);
+        }
+    }
+}
